662aec10d28e217c67027ad0878b19b21ae5e3c9
Caio Campos <caio.campos1204@gmail.com>
2019-10-27 11:16:33 -0300

    Ajuste de dependências

--- projects/caiocampos/links-buttons/karma.conf.js
+++ projects/caiocampos/links-buttons/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/campos/links-buttons'),
+      dir: require('path').join(__dirname, '../../../coverage/caiocampos/links-buttons'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
